b52922b62361a6e5c3125cf901047bdedcec2157
liming <827182486@qq.com>
2020-03-18 22:19:05 +0800

    进制转换

---
+++ 进制转换.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/18 22:14
+# @Author  : 一叶知秋
+# @File    : 进制转换.py
+# @Software: PyCharm
+"""
+
+写出一个程序，接受一个十六进制的数，输出该数值的十进制表示。（多组同时输入 ）
+
+输入描述:
+输入一个十六进制的数值字符串。
+
+输出描述:
+输出该数值的十进制字符串。
+
+示例1
+输入
+0xA
+输出
+10
+"""
+
+while True:
+    try:
+        print(int(input(), 16))
+    except:
+        break
