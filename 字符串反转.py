f968fa3447e1d42adbdc0a7690710b45561debe6
liming <827182486@qq.com>
2020-03-19 16:40:39 +0800

    字符串反转

---
+++ 字符串反转.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/19 16:39
+# @Author  : 一叶知秋
+# @File    : 字符串反转.py
+# @Software: PyCharm
+"""
+题目描述
+写出一个程序，接受一个字符串，然后输出该字符串反转后的字符串。（字符串长度不超过1000）
+
+输入描述:
+输入N个字符
+
+输出描述:
+输出该字符串反转后的字符串
+
+示例1
+输入
+复制
+abcd
+输出
+复制
+dcba
+"""
+
+
+def main():
+    print(input()[::-1])
+
+
+if __name__ == '__main__':
+    main()
